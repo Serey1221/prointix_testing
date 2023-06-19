23f817fce7f4353bdcfdb07a62f0e984f46aa980
PhengPenghak <95391236+PhengPenghak@users.noreply.github.com>
2023-06-19 14:57:35 +0700

    update icon footer and slide home page

--- web/js/index.js
+++ web/js/index.js
@@ -21,14 +21,13 @@ var swiper = new Swiper(".cart_slider", {
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 3,
   spaceBetween: 30,
-  loop: true,
-  centerSlider:'true',
+  centeredSlides: true,
   fade:'true',
   gragCursor:'true',
-  speed:2000,
-  autoplay:{
-    delay:20
-  },
+  speed:1500,
+  // autoplay:{
+  //   delay:20
+  // },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
@@ -37,5 +36,6 @@ var swiper = new Swiper(".mySwiper", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+  loop: true,
 });
 
